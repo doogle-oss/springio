6bf410b1f93b49fadd5f993f8ffa8580c2667a76
Debabrata Patnaik <debabrata_patnaik@live.com>
2026-05-15 21:45:08 +0530

    test case fixed with profile local.

--- src/test/java/org/doogleoss/springio/SpringioApplicationTests.java
+++ src/test/java/org/doogleoss/springio/SpringioApplicationTests.java
@@ -2,8 +2,10 @@ package org.doogleoss.springio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"local"})
 class SpringioApplicationTests {
 
 	@Test
