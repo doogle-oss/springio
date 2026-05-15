f26c48c02a1449ed9096f1609691ff41a97e7cf0
Debabrata Patnaik <debabrata_patnaik@live.com>
2026-05-15 21:54:05 +0530

    add dog details and controller/repo.

--- src/main/java/org/doogleoss/springio/dog/DogRepository.java
+++ src/main/java/org/doogleoss/springio/dog/DogRepository.java
@@ -1,5 +1,9 @@
 package org.doogleoss.springio.dog;
 
+import java.util.Collection;
 import org.springframework.data.repository.ListCrudRepository;
 
-interface DogRepository extends ListCrudRepository<Dog, Integer> {}
+interface DogRepository extends ListCrudRepository<Dog, Integer> {
+
+  Collection<Dog> findByName(String name);
+}
--- src/main/java/org/doogleoss/springio/dog/DogsController.java
+++ src/main/java/org/doogleoss/springio/dog/DogsController.java
@@ -2,6 +2,7 @@ package org.doogleoss.springio.dog;
 
 import java.util.Collection;
 import java.util.Map;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +17,7 @@ class DogsController {
     this.repository = repository;
   }
 
-  @GetMapping("/dogs")
+  @GetMapping(value = "/dogs", version = "1.0")
   Collection<Map<String, Object>> dogs() {
     return repository.findAll().stream()
         .map(
@@ -32,4 +33,29 @@ class DogsController {
                     (Object) dog.description()))
         .toList();
   }
+
+  @GetMapping(value = "/dogs", version = "1.1")
+  Collection<Map<String, Object>> dogsv11() {
+    return repository.findAll().stream()
+        .map(dog -> Map.of("id", dog.id(), "fullName", dog.name(), "owner", (Object) dog.owner()))
+        .toList();
+  }
+
+  @GetMapping(value = "/dogs/search", produces = "application/json")
+  Collection<Map<String, Object>> findByName(@RequestParam String name) {
+    return repository.findByName(name).stream()
+        .map(
+            dog ->
+                Map.of(
+                    "id",
+                    dog.id(),
+                    "fullName",
+                    dog.name(),
+                    "owner",
+                    dog.owner(),
+                    "description",
+                    (Object) dog.description()))
+        .toList();
+  }
+
 }
